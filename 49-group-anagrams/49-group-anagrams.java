b1999707a8b6f1f57060647eb34d50683e6111bd
Himanshu Goyal <goyal.himanshu@live.in>
2022-07-13 19:58:12 +0530

    Time: 15 ms (46.02%), Space: 55.3 MB (74.94%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -3,24 +3,15 @@ class Solution {
         List<List<String>> finalR = new ArrayList<>();
         Map<String, List<String>> result = new HashMap<>();
         int len = strs.length;
-        for(int i=0 ;i<len; i++){
+         for(int i=0 ;i<len; i++){
           String temp = strs[i];
           char[] mid = temp.toCharArray();
           Arrays.sort(mid);
           temp = new String(mid);
-          if(result.containsKey(temp)){
-            List<String> val = result.get(temp);
-            val.add(strs[i]);
-            result.put(temp, val);
-          } else{
-            List<String> val = new ArrayList<>();
-            val.add(strs[i]);
-            result.put(temp, val);
-          }
+          if(!result.containsKey(temp)) result.put(temp, new LinkedList<>());
+          result.get(temp).add(strs[i]);
         }
-        for(String key : result.keySet()){
-          finalR.add(result.get(key));
-        }
-        return finalR;
+        
+        return new LinkedList<>(result.values());
     }
 }
