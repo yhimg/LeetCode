ab607e13e8d114e7c50ba4932fc02704e03d55c1
Himanshu Goyal <goyal.himanshu@live.in>
2022-07-15 17:49:20 +0530

    Time: 1 ms (99.04%), Space: 44.5 MB (35.02%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -16,15 +16,15 @@
 class Solution {
     private boolean result = true;
     public boolean isBalanced(TreeNode root) {
-        check(root);
-        return result;
+        return check(root) == -1 ? false : true;
     }
      private int check(TreeNode root){
          if(root == null) return 0;
          int left = check(root.left);
          int right = check(root.right);
+         if(left == -1 || right == -1) return -1;
          if(Math.abs(left-right) > 1){
-             result = false;
+             return -1;
          }
          return 1 + Math.max(left, right);
          
