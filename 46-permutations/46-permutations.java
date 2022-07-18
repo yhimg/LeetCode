5a2e999e34cc705ef5f4818807e91640b366dd9b
Himanshu Goyal <goyal.himanshu@live.in>
2022-07-18 23:36:57 +0530

    Time: 2 ms (76.41%), Space: 44.6 MB (53.79%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,10 +1,52 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList();
-        List data = Arrays.asList(nums);
-        perm(nums, new ArrayList(), result, new ArrayList());
+        List<Integer> map = new ArrayList();
+        List<Integer> mid = new ArrayList();
+        
+        for(int i=0; i<nums.length; i++){
+            map.add(nums[i]);
+            
+        }
+        
+        permu(0, nums, result);
+        
+        
+        //perm(nums, new ArrayList(), result, new ArrayList());
+        
+        
+        
+        
         return result;
     }
+    
+    private void permu(int index, int[] arr, List<List<Integer>> result){
+
+        if(index == arr.length){
+            List<Integer> mid = new ArrayList();
+            for(int x: arr){
+                mid.add(x);
+            }
+            result.add(new ArrayList(mid));
+            return;
+        }
+            
+        for(int i = index; i< arr.length; i++){
+            
+            swap(i, index, arr);
+            permu(index+1, arr, result);
+            swap(i, index, arr);
+            //mid.add(Arrays.asList(arr));
+            
+        }
+    }
+    
+    private void swap(int i, int j, int[] arr){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
     
     private void perm(int[] arr, List<Integer> mid, List<List<Integer>> result, List<Integer> map){
         
