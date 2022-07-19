3b289fd58c547b2f2637aafbe6fcf38f4c6713dd
Himanshu Goyal <goyal.himanshu@live.in>
2022-07-19 12:18:08 +0530

    Time: 3 ms (48.03%), Space: 44 MB (64.02%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,31 +1,22 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList();
-        /*result.add(new ArrayList());
-        for(int n : nums){
-            int size = result.size();
-            for(int i=0; i<size; i++){
-                List<Integer> mid = new ArrayList(result.get(i));
-                mid.add(n);
-                if(!result.contains(mid)) result.add(mid);
-            }
-        }*/
-        subSeq(0, nums, result, new ArrayList());
+        if(nums == null || nums.length==0) return result;
+        Arrays.sort(nums);
+        subSeq(0, nums, new ArrayList(), result);
         return result;
     }
     
-    private void subSeq(int idx, int[] arr, List<List<Integer>> result, List<Integer> mid){
+    
+    private void subSeq(int index, int[] arr, List<Integer> mid, List<List<Integer>> result){
         
         result.add(new ArrayList(mid));
         
-        for(int i=idx; i< arr.length; i++){
-            if(i > idx && arr[i] == arr[i-1]) continue;
+        for(int i = index; i<arr.length; i++){
+            if(i > index && arr[i] == arr[i-1]) continue;
             mid.add(arr[i]);
-            subSeq(i+1, arr, result, mid);
+            subSeq(i+1, arr, mid, result);
             mid.remove(mid.size()-1);
         }
-        
-        //subSeq(idx+1, arr, result, mid);
     }
 }
