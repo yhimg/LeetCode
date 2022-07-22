ebad8be9799425450ed3a145088305252fc0220d
Himanshu Goyal <goyal.himanshu@live.in>
2022-07-22 15:44:56 +0530

    Time: 4 ms (43.97%), Space: 48 MB (28.34%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.java
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.java
@@ -15,8 +15,17 @@
  */
 class Solution {
     public List<Double> averageOfLevels(TreeNode root) {
-        return bfs(root);
-    }
+        //return bfs(root);
+        
+        List<Double> sum = new ArrayList();
+        List<Integer> count = new ArrayList();
+        List<Double> result = new ArrayList();
+        dfs(root, 0, sum, count);
+        for(int i = 0; i<sum.size(); i++){
+            result.add(sum.get(i)/count.get(i));
+        }
+        return result;
+     }
     
     private List<Double> bfs(TreeNode root){
         List<Double> ans = new ArrayList();
@@ -36,6 +45,22 @@ class Solution {
             ans.add((sum/size));
         }
         return ans;
+    }
+    
+    private void dfs(TreeNode root, int level, List<Double> sum, List<Integer> count){
+        if(root == null) return;
+        
+        if(level < sum.size()){
+            sum.set(level, sum.get(level)+root.val);
+            count.set(level, count.get(level) + 1);
+        } else{
+            sum.add(1.0 * root.val);
+            count.add(1);
+        }
+        
+        dfs(root.left, level+1, sum, count);
+        dfs(root.right, level+1, sum, count);
+        
         
     }
 }
