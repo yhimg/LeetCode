e2debde84a2f1e9c0221a8a10cfb3c5f2e5d8209
Himanshu Goyal <goyal.himanshu@live.in>
2022-07-26 18:04:24 +0530

    Time: 1 ms (63.16%), Space: 42.4 MB (38.95%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -15,48 +15,14 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        /*if(root == null) return true;
-        Queue<TreeNode> leftQ = new LinkedList();
-        Queue<TreeNode> rightQ = new LinkedList();
-        if(root.left != null) leftQ.add(root.left);
-        if(root.right != null) rightQ.add(root.right);
-        while(!(leftQ.isEmpty() || rightQ.isEmpty())){
-            int sizeL = leftQ.size();
-            TreeNode left = null;
-            TreeNode right = null;
-            for(int i=0; i<sizeL; i++){
-                left = leftQ.remove();
-                right = rightQ.remove();
-                if(left.val != right.val) return false;
-                if(left.left != null) leftQ.add(left.left);
-                if(right.left != null) leftQ.add(right.left);
-                if(right.right != null) rightQ.add(right.right);
-                if(left.right != null) rightQ.add(left.right);
-            }
-            if(leftQ.size() != rightQ.size()) break;;
-        }
-        if(leftQ.size() != rightQ.size()) return false;
-        return true;*/
-        
         if(root == null) return true;
-        Queue<TreeNode> q = new LinkedList();
-        q.add(root.left);
-        q.add(root.right);
-        while(!q.isEmpty()){
-            TreeNode left = q.remove();
-            TreeNode right = q.remove();
-            if(left == null && right == null) continue;
-            if(left == null || right == null || left.val != right.val) return false;
-            
-            q.add(left.left);
-            q.add(right.right);
-            q.add(left.right);
-            q.add(right.left);
-            
-        }
-        return true;
         
+        return isSymmetric(root.left, root.right);
     }
     
-    
+    private boolean isSymmetric(TreeNode rootL, TreeNode rootR){
+        if(rootL == null && rootR == null) return true;
+        if(rootL == null || rootR == null) return false;
+        return rootL.val == rootR.val && isSymmetric(rootL.left, rootR.right) && isSymmetric(rootL.right, rootR.left);
+    }
 }
