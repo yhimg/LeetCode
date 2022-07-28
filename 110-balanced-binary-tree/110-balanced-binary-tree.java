f74290531029f1b931a6d153428ea073483e4d62
Himanshu Goyal <goyal.himanshu@live.in>
2022-07-28 17:57:21 +0530

    Time: 2 ms (33.96%), Space: 44.3 MB (56.90%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -21,8 +21,9 @@ class Solution {
     private int maxDepth(TreeNode root){
         if(root == null) return 0;
         int left = maxDepth(root.left);
+        if(left == -1) return -1;
         int right = maxDepth(root.right);
-        if(left == -1 || right == -1) return -1;
+        if(right == -1) return -1;
         if(Math.abs(left-right) > 1) return -1;
         return 1 + Math.max(left, right); 
     }
