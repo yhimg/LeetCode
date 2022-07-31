dc4fddff05778872b6442d29a979caa381e2cc3a
Himanshu Goyal <goyal.himanshu@live.in>
2022-07-31 18:25:56 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (67.07%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -20,26 +20,10 @@ class Solution {
     
     private int solve(TreeNode root, int prev){
         int sum = 0;
-        if(root == null) {
-            return 0;
-        }
+        if(root == null) return sum;
         prev = (prev << 1) | root.val;
         if(root.left == null && root.right == null) sum +=prev;
         
         return sum + solve(root.left, prev) + solve(root.right, prev);
-    }
-    
-    
-    private int binaryToDecimal(int binary){
-        int decimal = 0;
-        int n = 0;
-        if(binary == 0) return 0;
-        while(binary > 0){
-            int temp = binary % 10;
-            decimal += temp * Math.pow(2,n); 
-            binary = binary/10;
-            n++;
-        }
-        return decimal;
     }
 }
