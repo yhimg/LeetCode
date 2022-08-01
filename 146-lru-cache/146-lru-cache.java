d2e8bcdd5b0e503f0858e3ff0739f1189dec6379
Himanshu Goyal <goyal.himanshu@live.in>
2022-08-01 16:43:34 +0530

    Time: 101 ms (43.38%), Space: 127.4 MB (44.30%) - LeetHub

---
+++ 146-lru-cache/146-lru-cache.java
@@ -0,0 +1,92 @@
+class LRUCache {
+    
+    static class Node{
+        int key, value;
+        Node next, prev;
+        Node(){
+            key = -1;
+            value = -1;
+            next = null;
+            prev = null;
+        }
+        
+        Node(int key, int value){
+            this.key = key;
+            this.value = value;
+            next = null;
+            prev = null;
+        }
+    }
+
+    
+    int cacheSize;
+    Map<Integer, Node> cache;
+    Node head;
+    Node tail;
+    public LRUCache(int capacity) {
+        cacheSize = capacity;
+        cache = new HashMap();
+        head = new Node();
+        tail = new Node();
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public int get(int key) {
+        if(cache.containsKey(key)){
+            Node temp = cache.get(key);
+            temp.prev.next = temp.next;
+            temp.next.prev = temp.prev;
+            temp.next = head.next;
+            head.next = temp;
+            temp.next.prev = temp;
+            temp.prev = head;
+            cache.put(key, temp);
+            return temp.value;
+        }
+        return -1;
+    }
+    
+    public void put(int key, int value) {
+        if(cache.containsKey(key)){
+            Node temp = cache.get(key);
+            temp.prev.next = temp.next;
+            temp.next.prev = temp.prev;
+            temp.value = value;
+            temp.next = head.next;
+            head.next = temp;
+            temp.next.prev = temp;
+            temp.prev = head;
+            cache.put(key, temp);
+        } else {
+            if(cache.size()<cacheSize){
+                Node temp = new Node(key, value);
+                temp.next = head.next;
+                head.next = temp;
+                temp.next.prev = temp;
+                temp.prev = head;
+                cache.put(key, temp);
+            } else{
+                Node delete = tail.prev;
+                tail.prev = tail.prev.prev;
+                tail.prev.next = tail;
+                delete.next = null;
+                delete.prev = null;
+                cache.remove(delete.key);
+                Node temp = new Node(key, value);
+                temp.next = head.next;
+                head.next = temp;
+                temp.next.prev = temp;
+                temp.prev = head;
+                cache.put(key, temp);
+            }
+        }
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
