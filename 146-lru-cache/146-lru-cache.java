120546d99b73bbe8bc1596f6fd25485a6c933486
Himanshu Goyal <goyal.himanshu@live.in>
2022-08-01 22:35:48 +0530

    Time: 129 ms (17.94%), Space: 127.6 MB (43.47%) - LeetHub

--- 146-lru-cache/146-lru-cache.java
+++ 146-lru-cache/146-lru-cache.java
@@ -1,23 +1,86 @@
 class LRUCache {
     
-    private LinkedHashMap<Integer, Integer> map;
-    private final int CAPACITY;
+    static class Node{
+        int key, value;
+        Node next, prev;
+        Node(){
+            key = -1;
+            value = -1;
+            next = null;
+            prev = null;
+        }
+        
+        Node(int key, int value){
+            this.key = key;
+            this.value = value;
+            next = null;
+            prev = null;
+        }
+    }
 
+    
+    int cacheSize;
+    Map<Integer, Node> cache;
+    Node head;
+    Node tail;
     public LRUCache(int capacity) {
-        CAPACITY = capacity;
-        map = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true){
-            protected boolean removeEldestEntry(Map.Entry eldest) {
-                return size() > CAPACITY;
-            }
-        };
+        cacheSize = capacity;
+        cache = new HashMap();
+        head = new Node();
+        tail = new Node();
+        head.next = tail;
+        tail.prev = head;
     }
     
     public int get(int key) {
-        return map.getOrDefault(key, -1);
+        if(cache.containsKey(key)){
+            Node temp = cache.get(key);
+            temp.prev.next = temp.next;
+            temp.next.prev = temp.prev;
+            pushAtBegin(temp);
+            cache.put(key, temp);
+            return temp.value;
+        }
+        return -1;
     }
     
     public void put(int key, int value) {
-        map.put(key, value);
+        if(cache.containsKey(key)){
+            Node temp = cache.get(key);
+            temp.prev.next = temp.next;
+            temp.next.prev = temp.prev;
+            temp.value = value;
+            pushAtBegin(temp);
+            cache.put(key, temp);
+        } else {
+            if(cache.size()<cacheSize){
+                Node temp = new Node(key, value);
+                pushAtBegin(temp);
+                cache.put(key, temp);
+            } else{
+                Node delete = deleteFromEnd();
+                cache.remove(delete.key);
+                Node temp = new Node(key, value);
+                pushAtBegin(temp);
+                cache.put(key, temp);
+            }
+        }
+    }
+    
+    private void pushAtBegin(Node temp){
+        temp.next = head.next;
+        head.next = temp;
+        temp.next.prev = temp;
+        temp.prev = head;
+    }
+    
+    private Node deleteFromEnd(){
+        Node delete = tail.prev;
+        tail.prev = tail.prev.prev;
+        tail.prev.next = tail;
+        delete.next = null;
+        delete.prev = null;
+        return delete;
     }
 }
 
