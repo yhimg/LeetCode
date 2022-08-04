b423483fd7346a78d63980c080b1898100e1d971
Himanshu Goyal <goyal.himanshu@live.in>
2022-08-04 21:40:39 +0530

    Time: 82 ms (28.78%), Space: 113.7 MB (21.10%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,10 +1,16 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);//nlogn
-        Set<List<Integer>> result = new HashSet();
+        List<List<Integer>> result = new ArrayList();
         for(int i = 0; i<nums.length; i++){//n
+            if(nums[i] > 0) break;
+            if(i > 0 && nums[i-1] == nums[i]) continue;
             int j = i+1, k = nums.length -1;
             while(j<k){//logn
+                if(j > i+1 && nums[j-1] == nums[j] ){
+                    j++;
+                    continue;
+                }
                 List<Integer> mid = new ArrayList();
                 int sum = nums[j] + nums[k] + nums[i];
                 if( sum == 0){
@@ -19,6 +25,39 @@ class Solution {
                 
             }
         }
-        return new ArrayList(result);
+        return result;
+        
+        
+        /*int i=0;
+        Arrays.sort(nums);//nlogn
+        List<List<Integer>> result = new HashSet();
+        
+        while(i<nums.length-1){
+            int j = i+1;
+            int sum = nums[i]+nums[j];
+            int low = j+1;
+            int high = nums.length -1;
+            List<Integer> midList = new ArrayList();
+            while(low <= high){
+                int mid = low + (high-low)/2;
+                if(sum+nums[mid] == 0){
+                    midList.add(nums[i]);
+                    midList.add(nums[j]);
+                    midList.add(nums[mid]);
+                    result.add(midList);
+                    low++;
+                    high--;
+                    result.add(midList);
+                } else if(sum + nums[mid] > 0){
+                    high = mid-1;
+                } else{
+                    low = mid+1;
+                }
+            }
+            
+            i++;
+            j++;
+        }
+        return result;*/
     }
 }
