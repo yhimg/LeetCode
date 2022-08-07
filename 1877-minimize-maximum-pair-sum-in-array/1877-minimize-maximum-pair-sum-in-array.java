321208ef98a5a961a12b0905ad1790c6e060fff9
Himanshu Goyal <goyal.himanshu@live.in>
2022-08-07 15:05:49 +0530

    Time: 81 ms (34.13%), Space: 106.5 MB (35.41%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
@@ -4,9 +4,7 @@ class Solution {
         int low = 0, high = nums.length-1;
         int max = 0;
         while(low<high){
-            max = Math.max(nums[low]+nums[high], max);
-            low++;
-            high--;
+            max = Math.max(nums[low++]+nums[high--], max);
         }
         return max;
     }
