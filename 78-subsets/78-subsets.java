1179e1d8cca93805b0084bf1c08fe472265c4e72
Himanshu Goyal <goyal.himanshu@live.in>
2022-08-08 16:49:06 +0530

    Time: 1 ms (89.35%), Space: 43.6 MB (34.35%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,41 +1,20 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList();
-        result.add(new ArrayList());
-        for(int n: nums){
-            int size = result.size();
-            for(int i=0; i<size; i++){
-                List<Integer> mid = new ArrayList(result.get(i));
-                mid.add(n);
-                result.add(mid);
-            }   
-        } 
-        //subSeq(0, nums, result, new ArrayList());
-        return result;
-    }
-    
-    private void subSeq(int index, int[] arr, List<List<Integer>> result, List<Integer> mid){
+    List<List<Integer>> list = new ArrayList<>();
+    Arrays.sort(nums);
+    backtrack(list, new ArrayList<>(), nums, 0);
+    return list;
+}
+
+private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start) {
+    if(start >= nums.length){
+        list.add(new ArrayList<>(tempList));
+        return;
+    } 
         
-        if(index > arr.length) return;
-        result.add(new ArrayList(mid));
-        
-        for(int i=index; i<arr.length; i++){
-            mid.add(arr[i]);
-            subSeq(i+1, arr, result, mid);
-            mid.remove(mid.size()-1);
-        }
-        
-        
-        
-        /*if(index == arr.length){
-            result.add(new ArrayList(mid));
-            return;
-        }
-        
-        mid.add(arr[index]);
-        subSeq(index+1, arr, result, mid);
-        mid.remove(mid.size()-1);
-        subSeq(index+1, arr, result, mid);*/
-        
-    }
+    tempList.add(nums[start]);
+    backtrack(list, tempList, nums, start + 1);
+    tempList.remove(tempList.size() - 1);
+    backtrack(list, tempList, nums, start + 1);
+}
 }
