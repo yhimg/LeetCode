1c53c8fd22d84bc0540b980cdc31665be0b78b69
Himanshu Goyal <goyal.himanshu@live.in>
2022-08-09 12:31:57 +0530

    Time: 4 ms (77.82%), Space: 45.6 MB (47.13%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,80 +1,65 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-       String[][] board = new String[n][n];
+        String[][] board = new String[n][n];
         for(int i = 0; i<n; i++){
-            for(int j=0; j<n; j++){
+            for(int j = 0; j<n; j++){
                 board[i][j] = ".";
             }
         }
-        //solve(0, n, result);
-        
         List<List<String>> result = new ArrayList();
-        int[] leftCol = new int[n];
-        int[] lowerDiag = new int[2*(n)-1];
-        int[] upperDiag = new int[2*(n)-1];
-        solve(0, n, board, result, leftCol, lowerDiag, upperDiag);
-        
-        
+        solve(0, n, board, result);
         return result;
     }
     
-    private void solve(int col, int n, String[][] board, List<List<String>> result,
-                      int[] leftCol, int[] lowerDiag, int[] upperDiag){
-        if(col >= n){
-            
-            List<String> mid = new ArrayList();
-            
+    private void solve(int col, int n, String[][] board, List<List<String>> result){
+        if(col < n){
             for(int i = 0; i<n; i++){
-                StringBuilder builder = new StringBuilder();
-                for(int j=0; j<n; j++){
-                    builder.append(board[i][j]);
+                if(isValidPos(i, col, n, board)){
+                    board[i][col] = "Q";
+                    solve(col+1, n, board, result);
+                    board[i][col] = ".";
                 }
-                mid.add(builder.toString());
             }
-            result.add(new ArrayList(mid));
-            
-        
+        } else{
+            List<String> mid = new ArrayList();
+            for(int i = 0; i<n; i++){
+                StringBuilder sb = new StringBuilder();
+                for(int j = 0; j<n; j++){
+                    sb.append(board[i][j]);
+                }
+                mid.add(sb.toString());
+                
+            }
+            result.add(mid);
             return;
-        }
-        
-        for(int row = 0; row < n; row++){
-            //if(validPlace(row, col, board, n)){
-            if(leftCol[row] == 0 && lowerDiag[row+col] == 0 && upperDiag[n-1+col-row] == 0){
-                board[row][col] = "Q";
-                leftCol[row] = 1;
-                lowerDiag[row+col] = 1;
-                upperDiag[n-1+col-row] = 1;
-                solve(col+1, n, board, result, leftCol, lowerDiag, upperDiag);
-                board[row][col] = ".";
-                leftCol[row] = 0;
-                lowerDiag[row+col] = 0;
-                upperDiag[n-1+col-row] = 0;
-            }
         }
     }
     
-    
-    private boolean validPlace(int row, int col, String[][] board, int n){
-        int tempRow = row;
-        int tempCol = col;
-         while(tempRow >=0 && tempCol >=0){
-             if(board[tempRow][tempCol] == "Q") return false;
-             tempRow--;
-             tempCol--;
-         }
-        tempRow = row;
-        tempCol = col;
+    private boolean isValidPos(int x, int y, int n, String[][] board){
+        int i = x;
+        int j = y;
         
-        while(tempCol >=0 ){
-            if(board[tempRow][tempCol] == "Q") return false;
-            tempCol--;
+        while(i >= 0 && j >= 0){
+            if(board[i][j] == "Q") return false;
+            i--;
+            j--;
         }
-        tempCol = col;
-        while(tempRow <n && tempCol >=0){
-             if(board[tempRow][tempCol] == "Q") return false;
-             tempRow++;
-             tempCol--;
-         }
+        
+        i=x;
+        j=y;
+        while(j >= 0){
+            if(board[i][j] == "Q") return false;
+            j--;
+        }
+        
+        i=x;
+        j=y;
+        while(i < n && j >=0){
+            if(board[i][j] == "Q") return false;
+            i++;
+            j--;
+        }
+        
         return true;
     }
 }
