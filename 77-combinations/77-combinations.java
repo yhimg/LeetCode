70097243074b3d24037876a415165758a9a55a4a
Himanshu Goyal <goyal.himanshu@live.in>
2022-08-10 16:00:44 +0530

    Time: 37 ms (48.78%), Space: 54.5 MB (71.85%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -1,26 +1,22 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> result = new ArrayList();
-        solve(1, k, new ArrayList(), result, n);
+       List<List<Integer>> result = new ArrayList();
+        solve(1, n, k, result, new ArrayList());
         return result;
         
     }
     
-    
-    
-    private void solve(int index, int k, List<Integer> mid, List<List<Integer>> result, int n){
-        
+    private void solve(int index, int n, int k, List<List<Integer>> result, List<Integer> mid){
         if(mid.size() == k){
             result.add(new ArrayList(mid));
             return;
         }
-        
         for(int i = index; i<=n; i++){
             mid.add(i);
-            solve(i+1, k, mid, result, n);
+            solve(i+1, n, k, result, mid);
             mid.remove(mid.size()-1);
         }
-        
-        
     }
+    
+    
 }
