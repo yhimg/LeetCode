36f8f864158718a7982053d3f54f2af81f74c637
Himanshu Goyal <goyal.himanshu@live.in>
2022-08-10 14:38:01 +0530

    Time: 6 ms (57.56%), Space: 44 MB (52.59%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -1,29 +1,28 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        List<List<Integer>> result = new ArrayList();
-        subSeq(0, candidates, target, new ArrayList(), result);
-        return result;
+        Set<List<Integer>> result = new HashSet();
+        solve(0, candidates, result, new ArrayList(), target, 0);
+        return new ArrayList(result);
     }
-
-    private void subSeq(int index, int[] arr, int target, List<Integer> mid, List<List<Integer>> result){
-        
-        if(target == 0){
-          result.add(new ArrayList(mid)); 
-          return;
-         }
-        
-       
-        
-        for(int i = index; i< arr.length; i++){
-            if(i > index && arr[i] == arr[i-1]) continue;
-            if(arr[i] > target) break;
-            mid.add(arr[i]);
-            subSeq(i+1, arr, target-arr[i], mid, result);
-            mid.remove(mid.size()-1);
+    
+    private void solve(int index, int[] arr, Set<List<Integer>> result, List<Integer> mid, int sum, int currSum){
+        if(currSum == sum){
+            result.add(new ArrayList(mid));
+            return;
         }
         
         
-        //subSeq(index+1, arr, target, mid, result);
+        for(int i = index; i<arr.length; i++){
+            if(i > index && arr[i] == arr[i-1]) continue;
+            
+            if(currSum +arr[i] > sum){
+                break;
+            }
+            mid.add(arr[i]);
+            solve(i+1, arr, result, mid, sum, currSum+arr[i]);
+            mid.remove(mid.size()-1);
+        }
+        
     }
 }
