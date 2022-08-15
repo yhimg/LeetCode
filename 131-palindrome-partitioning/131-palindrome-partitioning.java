f2490511a375a4f2e6bb73499dfb058fca193edb
Himanshu Goyal <goyal.himanshu@live.in>
2022-08-15 12:40:14 +0530

    Time: 30 ms (16.40%), Space: 193.9 MB (45.73%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList();
+        solve(s, result, new ArrayList());
+        return result;
+    }
+    
+    private void solve(String s, List<List<String>> result, List<String> mid){
+        if(0 == s.length()){
+            result.add(new ArrayList(mid));
+            return;
+        }
+        
+        String temp = "";
+        for(int i = 0; i<s.length(); i++){
+           temp = s.substring(0, i+1);
+            if(isPalindrome(temp)){
+                mid.add(temp);
+                solve(s.substring(i+1), result, mid);
+                mid.remove(mid.size()-1);  
+                temp = temp.substring(0, temp.length()-1);
+            } 
+        }
+        
+    }
+    
+    private boolean isPalindrome(String s){
+        if(s.length() == 1) return true;
+        int low = 0;
+        int high = s.length()-1;
+        while(low<high){
+            if(s.charAt(low) != s.charAt(high)) return false;
+            low++;
+            high--;
+        }
+        return true;
+    }
+    
+    
+}
