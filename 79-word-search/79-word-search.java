0044bbb133aab4e19640d46b9b26b1c9ba709165
Himanshu Goyal <goyal.himanshu@live.in>
2022-08-16 22:42:46 +0530

    Time: 151 ms (67.96%), Space: 42.4 MB (43.23%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,9 +1,8 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int[][] visited = new int[board.length][board[0].length];
         for(int i = 0; i<board.length; i++){
             for(int j = 0; j<board[i].length; j++){
-                if(board[i][j] == word.charAt(0) && solve(i, j, board, board.length, board[i].length, word, visited, 0)){
+                if(board[i][j] == word.charAt(0) && solve(i, j, board, board.length, board[i].length, word, 0)){
                     return true;
                 }
             }
@@ -11,18 +10,18 @@ class Solution {
         return false;
     }
     
-    private boolean solve(int row, int col, char[][] board, int m, int n, String word, int[][] visited, int index){
+    private boolean solve(int row, int col, char[][] board, int m, int n, String word,int index){
         if(index == word.length()) 
             return true;
-        if(row >= m || col >= n || row < 0 || col < 0 || visited[row][col] == 1 || board[row][col] != word.charAt(index)) 
+        if(row >= m || col >= n || row < 0 || col < 0 || board[row][col] != word.charAt(index)) 
             return false;
         
-        visited[row][col] = 1;
-        boolean result = solve(row, col+1, board, m, n, word, visited, index+1) 
-            || solve(row, col-1, board, m, n, word, visited, index+1)
-            || solve(row+1, col, board, m, n, word, visited, index+1)
-            || solve(row-1, col, board, m, n, word, visited, index+1);
-        visited[row][col] = 0;
+        board[row][col] = '*';
+        boolean result = solve(row, col+1, board, m, n, word, index+1) 
+            || solve(row, col-1, board, m, n, word, index+1)
+            || solve(row+1, col, board, m, n, word, index+1)
+            || solve(row-1, col, board, m, n, word, index+1);
+        board[row][col] = word.charAt(index);
         return result;
         
     }
