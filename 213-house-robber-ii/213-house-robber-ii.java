be77042d9c1c9c949cbcbeb0db26caab35628b74
Himanshu Goyal <goyal.himanshu@live.in>
2022-08-20 12:26:11 +0530

    Time: 4 ms (5.16%), Space: 41.8 MB (34.24%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int rob(int[] nums) {
+        int len = nums.length;
+        if(len == 1) return nums[0];
+        int prev1 = nums[0];
+        int prev2 = 0;
+        for(int i = 1; i<len-1; i++){
+            int pick = nums[i];
+            if(i>1) pick += prev2;
+            int notPick = prev1;
+            int temp = Math.max(pick, notPick);
+            prev2 = prev1;
+            prev1 = temp;
+        }
+        System.out.println(prev1);
+        int prev3 = nums[1];
+        int prev4 = 0;
+        for(int i = 2; i<len; i++){
+            int pick = nums[i] + prev4;
+            int notPick = prev3;
+            int temp = Math.max(pick, notPick);
+            prev4 = prev3;
+            prev3 = temp;
+        }
+        System.out.println(prev3);
+        return Math.max(prev1, prev3);
+    }
+}
