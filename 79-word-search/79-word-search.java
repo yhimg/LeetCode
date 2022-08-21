5023508db4aa59e0b33d36e8d39f8c03dac9f207
Himanshu Goyal <goyal.himanshu@live.in>
2022-08-21 21:56:14 +0530

    Time: 128 ms (82.07%), Space: 42.3 MB (43.23%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,31 +1,33 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
+        int rows = board.length;
+        int cols = board[0].length;
+        
+        boolean [][] visited = new boolean[rows][cols];
         for(int i = 0; i<board.length; i++){
-            for(int j = 0; j<board[i].length; j++){
-                if(board[i][j] == word.charAt(0) && solve(i, j, board, board.length, board[i].length, word, 0)){
-                    return true;
-                }
+            for(int j = 0; j<board[0].length; j++){
+                if(board[i][j] == word.charAt(0) && solve(i, j, board, visited, word, 0)) return true;
             }
         }
+        
         return false;
     }
     
-    private boolean solve(int row, int col, char[][] board, int m, int n, String word,int index){
-        if(index == word.length()) 
+    
+    private boolean solve(int row, int col, char[][] board, boolean[][] visited, String word, int index){
+        if(word.length() == index){
             return true;
-        if(row >= m || col >= n || row < 0 || col < 0 || board[row][col] != word.charAt(index)) 
+        }
+        if(row == board.length || col == board[0].length || row < 0 || col < 0 || visited[row][col] || board[row][col] != word.charAt(index)){
             return false;
+        }
         
-        board[row][col] = '*';
-        boolean result = solve(row, col+1, board, m, n, word, index+1) 
-            || solve(row, col-1, board, m, n, word, index+1)
-            || solve(row+1, col, board, m, n, word, index+1)
-            || solve(row-1, col, board, m, n, word, index+1);
-        board[row][col] = word.charAt(index);
+        visited[row][col] = true;
+        boolean result = solve(row, col+1, board, visited, word, index+1) 
+            || solve(row, col-1, board, visited, word, index+1) 
+            || solve(row+1, col, board, visited, word, index+1) 
+            || solve(row-1, col, board, visited, word, index+1);
+        visited[row][col] = false;
         return result;
-        
     }
-    
-    
-    
 }
