41b6deb4540d41683003bc662bb87f4b94668246
Himanshu Goyal <goyal.himanshu@live.in>
2022-08-21 16:46:55 +0530

    Time: 1 ms (64.07%), Space: 42.6 MB (22.59%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,12 +1,30 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int row = obstacleGrid.length;
+        /*int row = obstacleGrid.length;
         int col = obstacleGrid[0].length;
         int dp[][] = new int[row][col];
         for(int i = 0; i<row; i++){
             Arrays.fill(dp[i], -1);
         }
-        return solve(row-1, col-1, obstacleGrid, dp);
+        return solve(row-1, col-1, obstacleGrid, dp);*/
+        
+        int row = obstacleGrid.length;
+        int col = obstacleGrid[0].length;
+        int dp[][] = new int[row][col];
+        for(int i = 0; i<row; i++){
+            for(int j = 0; j<col; j++){
+                if(obstacleGrid[i][j] != 1){
+                   if(i == 0 && j == 0) dp[i][j] = 1;
+                    else{
+                        if(i > 0) dp[i][j] = dp[i-1][j];
+                        if(j > 0) dp[i][j] += dp[i][j-1];
+                    } 
+                }
+                //System.out.println(dp[i][j]);
+            }
+        }
+        return dp[row-1][col-1];
+        
     }
     
     
