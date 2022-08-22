2b1531bd6a3f0cc57de70f3e181b0eb1a7194d50
Himanshu Goyal <goyal.himanshu@live.in>
2022-08-22 21:06:00 +0530

    Time: 1 ms (100.00%), Space: 42 MB (11.67%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
+        double r = (double) (Math.log(n) / Math.log(4));
+        return r - ((int) r) == 0;
+    }
+}
