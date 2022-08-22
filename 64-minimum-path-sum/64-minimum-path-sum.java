9c0169b07cf8d1ff3151bb7a3e15d2bff8d54fde
Himanshu Goyal <goyal.himanshu@live.in>
2022-08-22 21:52:00 +0530

    Time: 6 ms (11.24%), Space: 46.8 MB (6.97%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,13 +1,35 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int m = grid.length;
+        /*int m = grid.length;
         int n = grid[0].length;
         
         int[][] dp = new int[m][n];
         for(int i = 0; i< m; i++){
             Arrays.fill(dp[i], -1);
         }
-        return solve(m-1, n-1, grid, dp);
+        return solve(m-1, n-1, grid, dp);*/
+        
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        //dp[0][0] = grid[0][0];
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                int min = Integer.MAX_VALUE;
+                if(i == 0 && j == 0) dp[i][j] = grid[0][0];
+                dp[i][j] = grid[i][j];
+                if(i > 0){
+                    min = Math.min(min, dp[i-1][j]);
+                }
+                if(j > 0){
+                    min = Math.min(min, dp[i][j-1]);
+                }
+                if(min != Integer.MAX_VALUE)
+                    dp[i][j] += min;
+            }
+        }
+        return dp[m-1][n-1];
+        
     }
     
     private int solve(int row, int col, int[][] grid, int[][] dp){
