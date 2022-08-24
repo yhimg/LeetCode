dec183520ae425890282cc1a603cc06c46830850
Himanshu Goyal <goyal.himanshu@live.in>
2022-08-24 23:20:38 +0530

    Time: 4 ms (64.11%), Space: 43.5 MB (82.31%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,14 +1,34 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         
-        int m = triangle.size();
+        /*int m = triangle.size();
         
         int[][] dp = new int[m][triangle.get(m-1).size()];
         for(int i = 0; i< m;i++){
             Arrays.fill(dp[i], -1);
         }
         
-        return solve(0, 0, triangle, m, dp);
+        return solve(0, 0, triangle, m, dp);*/
+        
+        int m = triangle.size();
+        
+        int[][] dp = new int[m][triangle.get(m-1).size()];
+        
+        for(int i = 0; i<triangle.get(m-1).size(); i++){
+            dp[m-1][i] = triangle.get(m-1).get(i);
+        }
+        
+        for(int i = m-1; i>=0; i--){
+            for(int j = triangle.get(i).size()-1; j>= 0 ; j--){
+                if(i == m-1) dp[m-1][j] = triangle.get(m-1).get(j);
+                else{
+                 dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i+1][j+1], dp[i+1][j]);
+                }
+            }
+        }
+        return dp[0][0];
+        
+        
     }
     
     private int solve(int i, int j, List<List<Integer>> triangle, int m, int[][] dp){
