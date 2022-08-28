65cd90b63cf8830c11ca684e540888db32382ef4
Himanshu Goyal <goyal.himanshu@live.in>
2022-08-28 23:13:06 +0530

    Time: 83 ms (44.59%), Space: 53.9 MB (63.81%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,14 +1,52 @@
 class Solution {
-    public boolean canPartition(int[] nums) {
+    
+    private boolean helper(int index, int sum, int target, int[] arr){
+        if(sum == target) return true;
+        
+        if(index < 0) return false;
+        
+        boolean take = false;
+        if(sum+arr[index] <= target){
+            take = helper(index - 1, sum+arr[index], target, arr);
+        }
+        boolean notTake = helper(index-1, sum, target, arr);
+        
+        return take || notTake;
+    }
+    
+    
+    
+    
+    public boolean canPartition(int[] arr) {
+        int n = arr.length;
         int sum = 0;
-        for(int x : nums){
+        for(int x : arr){
             sum += x;
         }
         if(sum %2 != 0 ) return false;
-        int[][] dp = new int[nums.length][sum+1];
-        for(int i = 0; i < nums.length; i++)
-            Arrays.fill(dp[i], -1);
-        return solve(0, sum/2, nums, dp);
+        //return helper(n-1, 0, sum, nums);
+        int k = sum/2;
+        
+        boolean dp[][] = new boolean[n][k+1];
+        
+        for(int ind = 0; ind < n; ind++)
+            dp[ind][0] = true;
+        
+        dp[0][k] = k == arr[0];
+        
+        for(int ind = 1; ind < n; ind++){
+            for(int target = 0; target <= k; target++){
+                boolean notTake = dp[ind-1][target];
+                boolean take = false;
+                if(arr[ind] <= target)
+                    take = dp[ind-1][target-arr[ind]];
+                
+                dp[ind][target] = take || notTake;
+            }
+        }
+        return dp[n-1][k];
+    
+        
     }
     
     private boolean solve(int index, int target, int[] arr, int[][] dp){
