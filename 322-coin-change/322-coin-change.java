16fdb1968c152353db4bb98f37ba8eee20541b1f
Himanshu Goyal <goyal.himanshu@live.in>
2022-09-04 22:52:23 +0530

    Time: 192 ms (8.68%), Space: 53.9 MB (22.91%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,49 +1,50 @@
 class Solution {
-    public int coinChange(int[] coins, int target) {
+    public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        /*int[][] dp = new int[n][amount+1];
-        for(int i = 0; i<n; i++){
-            Arrays.fill(dp[i], -1);
+        /*int count = solve(n-1, coins, amount);
+        if(count < (int) Math.pow(10, 9)) return count;
+        return -1;*/
+        
+        int[][] dp = new int[n][amount+1];
+        
+        for(int target = 0; target <= amount; target++){
+            if(target % coins[0] == 0) dp[0][target] = target/coins[0];
+            else dp[0][target] = (int) Math.pow(10, 9);   
         }
-        int count = solve(n-1, coins, amount, dp);
-        if(count >= (int)Math.pow(10,9)) return -1;
-        return count;*/
-        
-        
-        int[][] dp = new int[n][target+1];
-        for(int T = 0; T <= target; T++){
-            if(T % coins[0] == 0) dp[0][T] = T/coins[0];
-            else dp[0][T] = (int)Math.pow(10,9);
-        }
-        
         
         for(int i = 1; i<n; i++){
-            for(int T = 0; T<=target; T++){
-                int notTake = dp[i-1][T];
-                int take = (int)Math.pow(10,9);
-                if(coins[i] <= T){
-                    take = 1 + dp[i][T-coins[i]];
+            for(int target = 0; target<= amount; target++){
+                int notTake = dp[i-1][target];
+                int take = (int) Math.pow(10, 9);
+                if(coins[i] <= target){
+                    take = 1 + dp[i][target-coins[i]];
                 }
-                dp[i][T] = Math.min(take, notTake);
+                
+                dp[i][target] = Math.min(take, notTake);
+                
             }
         }
-        return dp[n-1][target] < (int)Math.pow(10,9) ? dp[n-1][target] : -1;
+        
+        int count = dp[n-1][amount];
+        if(count < (int) Math.pow(10, 9)) return count;
+        return -1;
+        
+        
+        
     }
     
-    private int solve(int index, int[] coins, int total, int[][] dp){
-        
+    private int solve(int index, int[] arr, int target){
         if(index == 0){
-            if(total % coins[index] == 0) return total / coins[index];
-            return (int)Math.pow(10,9);
+            if(target % arr[index] == 0) return target/arr[index];
+            return (int) Math.pow(10, 9);
         }
+        int notTake = solve(index-1, arr, target);
+        int take = (int) Math.pow(10, 9);
+        if(arr[index] <= target)
+            take = 1 + solve(index, arr, target-arr[index]);
         
-        if(dp[index][total] != -1) return dp[index][total];
-        int notTake = solve(index-1, coins, total, dp);
-        int take = (int)Math.pow(10,9);
-        if(coins[index] <= total){
-            take = 1 + solve(index, coins, total-coins[index], dp);
-        }
-        
-        return dp[index][total] = Math.min(take, notTake);
+        return Math.min(take, notTake);
     }
+    
+    
 }
