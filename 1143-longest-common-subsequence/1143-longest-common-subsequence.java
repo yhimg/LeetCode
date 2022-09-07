7455ae83be5c5530730d1e9482e4ecb8743ae7cf
Himanshu Goyal <goyal.himanshu@live.in>
2022-09-07 23:08:11 +0530

    Time: 39 ms (12.69%), Space: 43.2 MB (94.90%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -2,13 +2,12 @@ class Solution {
     public int longestCommonSubsequence(String s1, String s2) {
         int len1 = s1.length();
         int len2 = s2.length();
-        int[][] dp = new int[len1][len2];
+        int[][] dp = new int[len1+1][len2+1];
         for(int i = 0; i<len1; i++)
             Arrays.fill(dp[i], -1);
-        return solve(len1-1, len2-1, s1, s2, dp);
+        return solve(len1, len2, s1, s2, dp);
         
         /*for(int i = 0; i<len1; i++){
-            if(s1.charAt(i) == s2.charAt(0)) return dp[i][0] = 1;
             
         }
         
@@ -28,31 +27,19 @@ class Solution {
                 dp[ind1][ind2] = len;
             }
         }
-        for(int i =0; i<len1; i++){
-            System.out.println("Hello");
-            for(int j = 0; j<len2; j++){
-                System.out.print(dp[i][j] + " ");
-            }
-            System.out.println();
-        }
+        
         return dp[len1-1][len2-1];*/
         
     }
     
     
     private int solve(int ind1, int ind2, String s1, String s2, int[][] dp){
-        if(ind1 < 0 || ind2 < 0) return 0;
-        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
-        /*if(ind1 == 0 || ind2 == 0){
-            if(s1.charAt(ind1) == s2.charAt(ind2)) return 1;
-        }*/
-        int len = 0;
-        if(s1.charAt(ind1) == s2.charAt(ind2)){
-            return dp[ind1][ind2] = 1 + solve(ind1-1, ind2-1, s1, s2, dp);
+        if(ind1 <= 0 || ind2 <= 0) return 0;
+        if(dp[ind1-1][ind2-1] != -1) return dp[ind1-1][ind2-1];
+        if(s1.charAt(ind1-1) == s2.charAt(ind2-1)){
+            return dp[ind1-1][ind2-1] = 1 + solve(ind1-1, ind2-1, s1, s2, dp);
         } else{
-            return dp[ind1][ind2]=  Math.max(solve(ind1, ind2-1, s1, s2, dp), solve(ind1-1, ind2, s1, s2, dp));
+            return dp[ind1-1][ind2-1]=  Math.max(solve(ind1, ind2-1, s1, s2, dp), solve(ind1-1, ind2, s1, s2, dp));
         }
-        
-        //return dp[ind1][ind2] = len;
     }
 }
