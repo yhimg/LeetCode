22bb0c9f1aeb4839ad35f9722ab9dd966adf8645
Himanshu Goyal <goyal.himanshu@live.in>
2022-09-07 19:28:36 +0530

    Time: 30 ms (23.04%), Space: 46.9 MB (17.20%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,11 +1,32 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        int len1 = text1.length();
-        int len2 = text2.length();
+    public int longestCommonSubsequence(String s1, String s2) {
+        int len1 = s1.length();
+        int len2 = s2.length();
         int[][] dp = new int[len1][len2];
         for(int i = 0; i<len1; i++)
             Arrays.fill(dp[i], -1);
-        return solve(len1-1, len2-1, text1, text2, dp);
+        return solve(len1-1, len2-1, s1, s2, dp);
+        
+        /*for(int i = 0; i<len1; i++){
+            if(s1.charAt(i) == s2.charAt(0)) return dp[i][0] = 1;
+            if(s1.charAt(0) == s2.charAt(i)) return dp[0][i] = 1;
+        }
+        
+        for(int ind1 = 1; ind1<len1; ind1++){
+            for(int ind2 = 1; ind2<len2; ind2++){
+                int len = 0;
+                int max = 0;
+                if(s1.charAt(ind1) == s2.charAt(ind2)){
+                    len = 1 + dp[ind1-1][ind2-1];
+                } else{
+                    max = Math.max(dp[ind1-1][ind2], dp[ind1][ind2-1]);
+                }
+                
+                dp[ind1][ind2] = len+max;
+            }
+        }
+        return dp[len1-1][len2-1];*/
+        
     }
     
     
@@ -18,11 +39,11 @@ class Solution {
         int len = 0;
         int max = 0;
         if(s1.charAt(ind1) == s2.charAt(ind2)){
-            len += 1 + solve(ind1-1, ind2-1, s1, s2, dp);
+            len = 1 + solve(ind1-1, ind2-1, s1, s2, dp);
         } else{
-            max =  Math.max(solve(ind1, ind2-1, s1, s2, dp), solve(ind1-1, ind2, s1, s2, dp));
+            len =  Math.max(solve(ind1, ind2-1, s1, s2, dp), solve(ind1-1, ind2, s1, s2, dp));
         }
         
-        return dp[ind1][ind2] = len+max;
+        return dp[ind1][ind2] = len;
     }
 }
