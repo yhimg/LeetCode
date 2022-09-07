4f71ab40c36bf6acd759b601c5e6b117a9273cca
Himanshu Goyal <goyal.himanshu@live.in>
2022-09-07 22:00:08 +0530

    Time: 27 ms (30.51%), Space: 46.5 MB (36.53%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -43,9 +43,9 @@ class Solution {
     private int solve(int ind1, int ind2, String s1, String s2, int[][] dp){
         if(ind1 < 0 || ind2 < 0) return 0;
         if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
-        if(ind1 == 0 || ind2 == 0){
+        /*if(ind1 == 0 || ind2 == 0){
             if(s1.charAt(ind1) == s2.charAt(ind2)) return 1;
-        }
+        }*/
         int len = 0;
         if(s1.charAt(ind1) == s2.charAt(ind2)){
             return dp[ind1][ind2] = 1 + solve(ind1-1, ind2-1, s1, s2, dp);
