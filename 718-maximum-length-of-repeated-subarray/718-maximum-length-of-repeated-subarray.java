ccad71837ba741e245f632508ffa91f2f5cc05c3
Himanshu Goyal <goyal.himanshu@live.in>
2022-09-09 13:44:47 +0530

    Time: 74 ms (64.14%), Space: 72.3 MB (74.11%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -3,15 +3,17 @@ class Solution {
         int len1 = nums1.length;
         int len2 = nums2.length;
         
-        int dp[][] = new int[len1+1][len2+1];
+        int prev[] = new int[len2+1];
         int max = 0;
         for(int i = 1; i<=len1; i++){
+            int[] curr = new int[len2+1];
             for(int j = 1; j<=len2; j++){
                 if(nums1[i-1] == nums2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                    max = Math.max(max, dp[i][j]);
+                    curr[j] = 1 + prev[j-1];
+                    max = Math.max(max, curr[j]);
                 }
             }
+            prev = curr;
         }
         return max;
     }
