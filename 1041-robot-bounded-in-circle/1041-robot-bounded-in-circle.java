9b60a6aafed36419dccc464c4c872a8d27030cbe
Himanshu Goyal <goyal.himanshu@live.in>
2022-09-10 16:33:25 +0530

    Time: 1 ms (76.26%), Space: 41.6 MB (78.12%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.java
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.java
@@ -1,20 +1,37 @@
 class Solution {
-    public boolean isRobotBounded(String str) {
-        
-        int i = 0;
-        int[][] dir = new int[][] {{0,1}, {-1,0}, {0, -1}, {1,0}};
-        int x = 0;
-        int y = 0;
-        for(int s = 0; s<str.length(); s++){
-            if(str.charAt(s) == 'L'){
-                i = (i+1)%4;
-            } else if(str.charAt(s) == 'R'){
-                i = (i+3)%4;
-            } else{
-                x = x + dir[i][0];
-                y = y + dir[i][1];
-            }
+    public boolean isRobotBounded(String instructions) {
+        return solve(0, 0, instructions, 0, 1);
+    }
+    
+    private boolean solve(int x, int y, String ins, int index, int direction){
+        if(index == ins.length()){
+            if(x == 0 && y == 0) return true;
+            return (direction != 1);
+            //return false;
         }
-        return x==0 && y==0 || i!=0;
+        boolean result = false;
+        if(ins.charAt(index) == 'L'){
+            direction -= 1;
+            if(direction == 0)
+                direction = 4;
+            result = solve(x, y, ins, index+1, direction);
+        }
+        else if(ins.charAt(index) == 'R'){
+            direction += 1;
+            if(direction > 4)
+                direction = 1;
+            result = solve(x, y, ins, index+1, direction);
+        } else{
+            if(direction == 1)
+                result = solve(x, y+1, ins, index+1, direction);
+            else if(direction == 2)
+                result = solve(x+1, y, ins, index+1, direction);
+            else if(direction == 3)
+                result = solve(x, y-1, ins, index+1, direction);
+            else if(direction == 4)
+                result = solve(x-1, y, ins, index+1, direction);
+        }
+        
+        return result;
     }
 }
