1129a95f5c38f84274af29a14d33a65a83f02533
Himanshu Goyal <goyal.himanshu@live.in>
2022-09-14 12:13:40 +0530

    Time: 26 ms (65.61%), Space: 42.2 MB (93.33%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -2,18 +2,31 @@ class Solution {
     public int numDistinct(String s, String t) {
         int n = s.length();
         int m = t.length();
-        int[][] dp = new int[n+1][m+1];
+        /*int[][] dp = new int[n+1][m+1];
         for(int i = 0; i<= n; i++)
             Arrays.fill(dp[i], -1);
-        return solve(n-1, m-1, s, t, dp);
+        return solve(n, m, s, t, dp);*/
+        
+        int[] curr = new int[m+1];
+        curr[0] = 1;
+            
+        
+        for(int i = 1; i<=n; i++){
+            for(int j = m; j>=1; j--){
+                if(t.charAt(j-1) == s.charAt(i-1))
+                    curr[j] = curr[j-1] + curr[j];
+            }
+        }
+        return curr[m];
+        
     }
     
     private int solve(int i, int j, String s, String t, int[][] dp){
-        if(j < 0) return 1;
-        if(i < 0) return 0;
+        if(j == 0) return 1;
+        if(i == 0) return 0;
         
         if(dp[i][j] != -1) return dp[i][j];
-        if(t.charAt(j) == s.charAt(i)){
+        if(t.charAt(j-1) == s.charAt(i-1)){
             return dp[i][j] = solve(i-1, j-1, s, t, dp) + solve(i-1, j, s, t, dp);
         }
         else{
