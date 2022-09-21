e457a9902708d1b442cf65521f51f6e0475b7614
Himanshu Goyal <goyal.himanshu@live.in>
2022-09-21 11:18:15 +0530

    Time: 406 ms (5.46%), Space: 142.6 MB (7.83%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -5,19 +5,19 @@ class Solution {
         for(int i = 0; i<n; i++)
             for(int j = 0; j < 2; j++)
                 Arrays.fill(dp[i][j], -1);
-        return solve(0, prices, 0, 1, dp);
+        return solve(0, prices, 2, 1, dp);
         
     }
     
     private int solve(int i, int[] arr, int count, int buy, int[][][] dp){
         
-        if(count == 2) return 0;
+        if(count == 0) return 0;
         if(i == arr.length) return 0;
         if(dp[i][buy][count] != -1) return dp[i][buy][count];
         if(buy == 1){
             return dp[i][buy][count] = Math.max((-arr[i] + solve(i+1, arr, count, 0, dp)), solve(i+1, arr, count, 1, dp));
         } else{
-            return dp[i][buy][count] = Math.max((arr[i] + solve(i+1, arr, count+1, 1, dp)), solve(i+1, arr, count, 0, dp));
+            return dp[i][buy][count] = Math.max((arr[i] + solve(i+1, arr, count-1, 1, dp)), solve(i+1, arr, count, 0, dp));
         }
     }
 }
