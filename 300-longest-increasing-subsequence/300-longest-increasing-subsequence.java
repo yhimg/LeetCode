c0350f497eabbffa6864be4b52e6868ce790c742
Himanshu Goyal <goyal.himanshu@live.in>
2022-09-24 12:37:36 +0530

    Time: 249 ms (10.23%), Space: 242.7 MB (5.09%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,20 +1,36 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int dp[][] = new int[n][n+1];
-        for(int i = 0; i<n; i++)
-            Arrays.fill(dp[i], -1);
-        return solve(0, n, nums, dp);
+    public int lengthOfLIS(int[] arr) {
+        // int n = nums.length;
+        // int dp[][] = new int[n][n+1];
+        // for(int i = 0; i<n; i++)
+        //     Arrays.fill(dp[i], -1);
+        // return solve(0, -1, nums, dp);
+        
+        int n = arr.length;
+        int dp[][] = new int[n+1][n+1];
+        for(int i = n-1; i>=0; i--){
+            for(int j = i-1; j>=-1; j--){
+                int len = dp[i+1][j+1];
+                if(j == -1 || arr[i] > arr[j])
+                    len = Math.max(len, 1 + dp[i+1][i+1]);
+                dp[i][j+1] = len;
+            }
+        }
+        
+        return dp[0][0];
+        
+        
+        
     }
     
     private int solve(int i, int prev_i, int[] arr, int[][] dp){
         if(i == arr.length) return 0;
-        if(dp[i][prev_i] != -1) return dp[i][prev_i];
+        if(dp[i][prev_i+1] != -1) return dp[i][prev_i];
         int take = 0;
-        if(prev_i == arr.length || arr[i] > arr[prev_i])
+        if(prev_i == -1 || arr[i] > arr[prev_i])
             take = 1 + solve(i+1, i, arr, dp);
         int notTake = solve(i+1, prev_i, arr, dp);
         
-        return dp[i][prev_i] = Math.max(take, notTake);
+        return dp[i][prev_i+1] = Math.max(take, notTake);
     }
 }
