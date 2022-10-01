ca5e8c09bed32683016da7f64a41c9401c22bbca
Himanshu Goyal <goyal.himanshu@live.in>
2022-10-01 18:53:44 +0530

    Time: 16 ms (81.98%), Space: 47 MB (12.91%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -5,14 +5,11 @@ class Solution {
         int[][] dp = new int[m+1][n+1];
         for(int i = 1; i<=m; i++){
             for(int j = 1; j<=n; j++){
-                int pick = 0;
-                int notPick = 0;
                 if(s1.charAt(i-1) == s2.charAt(j-1))
-                    pick = 1 + dp[i-1][j-1];
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 else
-                    notPick = Math.max(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 
-                dp[i][j] = pick + notPick;
             }
         }
         return dp[m][n];
