527cded47db488cd8c16519e00aab9085cc12f5c
Himanshu Goyal <goyal.himanshu@live.in>
2022-10-03 18:02:53 +0530

    Time: 7 ms (39.14%), Space: 57.8 MB (31.27%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -3,10 +3,17 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int count = 0;
+        int[][] visited = new int[m][n];
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
+                //if(grid[i][j] == '1' && visited[i][j] != 1){
                 if(grid[i][j] == '1'){
+                    // queue.add(new int[]{i, j});
+                    // visited[i][j] = 1;
                     count++;
+                    //bfs(visited, grid, m, n, queue);
+                    //dfs1(i, j, visited, grid, m, n);
                     dfs2(i, j, grid);
                 }
             }
@@ -21,5 +28,31 @@ class Solution {
         dfs2(i+1, j, grid);
         dfs2(i, j-1, grid);
         dfs2(i-1, j, grid);
+    }
+    private void dfs1(int i, int j, int[][] visited, char[][] grid, int m, int n){
+        if(i < 0 || j < 0 || i == m || j == n || grid[i][j] != '1' || visited[i][j] == 1)
+            return;
+        visited[i][j] = 1;
+        dfs1(i, j+1, visited, grid, m, n);
+        dfs1(i+1, j, visited, grid, m, n);
+        dfs1(i, j-1, visited, grid, m, n);
+        dfs1(i-1, j, visited, grid, m, n);
+    }
+    
+    
+    
+    private void bfs(int[][] visited, char[][] grid, int m, int n, ArrayDeque<int[]> queue){
+        int[][] dirn = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        while(!queue.isEmpty()){
+            int[] curr = queue.poll();
+            for(int[] dir : dirn){
+                int ni = curr[0] + dir[0];
+                int nj = curr[1] + dir[1];
+                if(ni >= 0 && nj >= 0 && ni < m && nj < n && grid[ni][nj] == '1' && visited[ni][nj] != 1) {
+                    queue.addLast(new int[]{ni, nj});
+                    visited[ni][nj] = 1;
+                }
+            }
+        }
     }
 }
