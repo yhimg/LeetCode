5761a973512f5a08a09bfe9ac10fd730a3e02c65
Himanshu Goyal <goyal.himanshu@live.in>
2022-12-01 22:46:44 +0530

    Time: 25 ms (62.32%), Space: 46.5 MB (34.55%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -2,11 +2,29 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length();
         int n = text2.length();
-        int[][] dp = new int[m][n];
-        for(int i = 0; i<m; i++){
-            Arrays.fill(dp[i], -1);
+        int[][] dp = new int[m+1][n+1];
+        
+        
+        for(int i = 1; i<=m; i++){
+            for(int j = 1; j<=n; j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
         }
-        return solve(m-1, n-1, text1, text2, dp);
+        
+        return dp[m][n];
+        
+        
+        // int m = text1.length();
+        // int n = text2.length();
+        // int[][] dp = new int[m][n];
+        // for(int i = 0; i<m; i++){
+        //     Arrays.fill(dp[i], -1);
+        // }
+        // return solve(m-1, n-1, text1, text2, dp);
     }
     
     private int solve(int ind1, int ind2, String s1, String s2, int[][] dp){
