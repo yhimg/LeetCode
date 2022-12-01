4624b20677826ed1edb2684fdae8130f9659ce83
Himanshu Goyal <goyal.himanshu@live.in>
2022-12-01 18:27:22 +0530

    Time: 93 ms (5.29%), Space: 45.7 MB (93.24%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,33 +1,20 @@
 class Solution {
-    public int longestCommonSubsequence(String s1, String s2) {
-        int m = s1.length();
-        int n = s2.length();
-        int[][] dp = new int[m+1][n+1];
-        for(int i = 1; i<=m; i++){
-            for(int j = 1; j<=n; j++){
-                if(s1.charAt(i-1) == s2.charAt(j-1))
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                else
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                
-            }
-        }
-        return dp[m][n];
-        //return solve(s1.length(), s2.length(), s1, s2);
-        
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m][n];
+        return solve(m-1, n-1, text1, text2, dp);
     }
     
-    int solve(int i, int j, String str1, String str2){
-        if(i == 0 || j == 0){
-          return 0;
-        }
-        int pick = 0;
-        int notPick = 0;
-        if(str1.charAt(i-1) == str2.charAt(j-1)){
-          pick = 1 + solve(i-1, j-1, str1, str2);
+    private int solve(int ind1, int ind2, String s1, String s2, int[][] dp){
+        if(ind1 < 0 || ind2 < 0) return 0;
+        if(dp[ind1][ind2] != 0) return dp[ind1][ind2];
+        int len = 0;
+        if(s1.charAt(ind1) == s2.charAt(ind2)){
+            len = 1 + solve(ind1-1, ind2-1, s1, s2, dp);
         } else{
-          notPick = Math.max(solve(i-1, j, str1, str2), solve(i, j-1, str1, str2));
+            len = Math.max(solve(ind1-1, ind2, s1, s2, dp), solve(ind1, ind2-1, s1, s2, dp));
         }
-        return pick + notPick;
-  }
+        return dp[ind1][ind2] = len;
+    }
 }
