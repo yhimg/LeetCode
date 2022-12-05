90bad07b161e5e3debe275638f46f1d2bed67ed6
Himanshu Goyal <goyal.himanshu@live.in>
2022-12-05 17:43:17 +0530

    Time: 57 ms (7.20%), Space: 50.6 MB (45.38%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -4,14 +4,39 @@ class Solution {
         for(int num : nums){
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        List<Integer> keys = new ArrayList(map.keySet());
         
-        Collections.sort(keys, (a, b) -> map.get(b)-map.get(a));
+        List<Integer>[] buckets = new ArrayList[nums.length + 1];
+        for(int key : map.keySet()){
+            System.out.println(key);
+            if(buckets[map.get(key)] == null)
+                buckets[map.get(key)] = new ArrayList();
+            buckets[map.get(key)].add(key);
+        }
+        
+        int i = buckets.length - 1;
         int[] ans = new int[k];
-        for(int i =0; i<k; i++){
-            ans[i] = keys.get(i);
+        int count = 0;
+        while(count < k){
+            if(buckets[i] != null){
+                for(int x : buckets[i]){
+                    if(count == k) break;
+                    ans[count] = x;
+                    count++;
+                }
+            }
+            i--;
         }
         return ans;
+        
+        
+//         List<Integer> keys = new ArrayList(map.keySet());
+        
+//         Collections.sort(keys, (a, b) -> map.get(b)-map.get(a));
+//         int[] ans = new int[k];
+//         for(int i =0; i<k; i++){
+//             ans[i] = keys.get(i);
+//         }
+//         return ans;
         
         
     }
