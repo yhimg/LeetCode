f5c37e6bbfbadf43a18cbb02743b8dbd85bec65a
Himanshu Goyal <goyal.himanshu@live.in>
2022-12-14 18:56:35 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (97.00%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -4,19 +4,7 @@ class Solution {
         int dp[] = new int[len];
         
         
-//         for(int i = 0; i<len; i++){
-//             int robCurr = 0;
-//             int notRobCurr = 0;
-//             if(i >= 2){
-//                 robCurr = nums[i] + dp[i-2];
-//             }
-//             if(i >= 1){
-//                 notRobCurr = dp[i-1];
-//             }
-//             dp[i] = Math.max(robCurr, notRobCurr);
-//         }
         
-//         return dp[len-1];
         
         Arrays.fill(dp, -1);
         return solve(len-1, nums, dp);
@@ -24,6 +12,8 @@ class Solution {
     
     private int solve(int ind, int[] arr, int[] dp){
         if(ind < 0) return 0;
+        
+        if(ind == 0) return arr[0];
         
         if(dp[ind] != -1) return dp[ind];
         
