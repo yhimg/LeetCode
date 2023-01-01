e38399c01edf40087ab7d4f82e74371e1a5abec2
Himanshu Goyal <goyal.himanshu@live.in>
2023-01-01 14:40:15 +0530

    Time: 1 ms (95.36%), Space: 39.9 MB (95.05%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if(words.length != pattern.length()) return false;
+        
+        Map<Character, String> map1 = new HashMap();
+        Map<String, Boolean> map2 = new HashMap();
+        
+        for(int i = 0; i<words.length; i++){
+            char mapWith = pattern.charAt(i);
+            if(map2.containsKey(words[i])){
+                if(!map1.containsKey(mapWith)) return false;
+                if(!map1.get(mapWith).equals(words[i])) return false;
+                
+            } else{
+                if(map1.containsKey(mapWith)) return false;
+                map2.put(words[i], true);
+                map1.put(mapWith, words[i]);
+            }
+        }
+        return true;
+    }
+}
