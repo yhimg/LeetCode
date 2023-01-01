1f586efbc8f655672c3d975a073b253771906e4c
Himanshu Goyal <goyal.himanshu@live.in>
2023-01-01 14:56:41 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (74.62%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -8,15 +8,28 @@ class Solution {
         
         for(int i = 0; i<words.length; i++){
             char mapWith = pattern.charAt(i);
-            if(map2.containsKey(words[i])){
-                if(!map1.containsKey(mapWith)) return false;
-                if(!map1.get(mapWith).equals(words[i])) return false;
-                
+            if(map1.containsKey(mapWith)){
+                String mWith = map1.get(mapWith);
+                if(!mWith.equals(words[i])) return false;
             } else{
-                if(map1.containsKey(mapWith)) return false;
-                map2.put(words[i], true);
+                if(map2.containsKey(words[i])) return false;
                 map1.put(mapWith, words[i]);
+                map2.put(words[i], true);
             }
+            
+            
+            
+            
+            
+//             if(map2.containsKey(words[i])){
+//                 if(!map1.containsKey(mapWith)) return false;
+//                 if(!map1.get(mapWith).equals(words[i])) return false;
+                
+//             } else{
+//                 if(map1.containsKey(mapWith)) return false;
+//                 map2.put(words[i], true);
+//                 map1.put(mapWith, words[i]);
+//             }
         }
         return true;
     }
