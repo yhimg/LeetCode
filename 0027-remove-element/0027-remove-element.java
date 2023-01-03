21288c5d26133b3cf124179c6f805fd875492eeb
Himanshu Goyal <goyal.himanshu@live.in>
2023-01-03 15:49:39 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (64.71%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -2,15 +2,24 @@ class Solution {
     public int removeElement(int[] nums, int val) {
         int len = nums.length;
         int i = 0, j= 0;
+        // while(j<len){
+        //     if(nums[j] != val){
+        //         if(j > i){
+        //             nums[i] = nums[j];
+        //         }
+        //         i++;
+        //     }
+        //     j++;
+        // }
+        
         while(j<len){
             if(nums[j] != val){
-                if(j > i){
-                    nums[i] = nums[j];
-                }
+                nums[i] = nums[j];
                 i++;
             }
             j++;
         }
+        
         return i;
     }
 }
